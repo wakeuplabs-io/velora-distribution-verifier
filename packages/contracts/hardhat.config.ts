56eaf551215af4b63f3bd309a171abafc20c97a6
Matias Zapata <matuzapata@gmail.com>
2025-05-08 13:19:10 -0500

    deployment and docs

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -1,8 +1,30 @@
+import "dotenv/config";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "https://ethereum-sepolia-rpc.publicnode.com",
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY!],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      'sepolia': process.env.SEPOLIA_ETHERSCAN_API_KEY!,
+    },
+    customChains: [
+      {
+        network: "sepolia",
+        chainId: 11155111,
+        urls: {
+          apiURL: "https://eth-sepolia.blockscout.com/api",
+          browserURL: "https://eth-sepolia.blockscout.com"
+        }
+      }
+    ]
+  }
 };
 
 export default config;
